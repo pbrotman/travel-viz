02340f6a06eeea0184b2d5c2fc62623e082c20fa
pbrotman <parkerbrotman@gmail.com>
2024-03-08 16:35:19 +0700

    Label code sections

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
-// var map = L.map('map').setView([51.505, -0.09], 13);
+/* MAP */
+
+// Instantiate map
 var map = L.map('map');
 
+// Define map starting bounds
 map.fitBounds([
     [21.904, 96.059],
     [-9.819, 119.853]
 ]).zoomIn(1)
 
+// Get map tiles
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-
+// Plot a circle (test)
 var circle = L.circle([3.1489, 101.6926], {
     color: 'red',
     fillColor: '#f03',
