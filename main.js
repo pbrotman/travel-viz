b379eaaa90ee721356a87f1b4541b8c4debcef50
pbrotman <parkerbrotman@gmail.com>
2024-03-08 14:31:54 +0700

    Initializing from Leaflet quick start guide

---
+++ main.js
@@ -0,0 +1,13 @@
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var circle = L.circle([51.508, -0.11], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(map);
