3a387cd30207945878800e4a5f068a7173501a10
pbrotman <parkerbrotman@gmail.com>
2024-07-08 16:06:41 -0400

    Commit locations data.

---
+++ data/locations.js
@@ -0,0 +1,34 @@
+let locations = [
+        // {
+        // "name": "example",
+        // "coordinates": {
+        //     "lat": 0,
+        //     "long": 0
+        // },
+        // "country": "example"
+        // },
+        {
+        "name": "Kuala Lumpur",
+        "coords": {
+            "lat": 3.1319,
+            "long": 101.6841
+        },
+        "country": "Malaysia"
+        },
+        {
+        "name": "Malacca",
+        "coords": {
+            "lat": 2.1896,
+            "long": 102.2501
+        },
+        "country": "Malaysia"
+        },
+        {
+        "name": "Singapore",
+        "coords": {
+            "lat": 1.3521,
+            "long": 103.8198
+        },
+        "country": "Singapore"
+        },
+    ];
