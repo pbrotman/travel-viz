424f21596507a077ddcdd5f08aed576a884d3cb1
pbrotman <parkerbrotman@gmail.com>
2024-07-08 15:55:20 -0400

    Read location data, plot locations on map.

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# travel-viz
+# Travel viz
+
+## Resources
 
 https://leafletjs.com/examples/quick-start/
+
+## Design decisions
+
+Due to the limited scope of the data, and in order to maintain simplicity, I opted to store the data as json objects in external javascript files.
--- main.js
+++ main.js
@@ -15,10 +15,24 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-// Plot a circle (test)
-var circle = L.circle([3.1489, 101.6926], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 500
-}).addTo(map);
+// // Plot a circle (test)
+// var circle = L.circle([3.1489, 101.6926], {
+//     color: 'red',
+//     fillColor: '#f03',
+//     fillOpacity: 0.5,
+//     radius: 500
+// }).addTo(map);
+
+
+// Plot locations
+let locationCircles = locations.map((loc) => {
+    let locCircle = L.circle([loc.coords.lat, loc.coords.long], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 500
+    }).addTo(map);
+    return(locCircle);
+});
+
+console.log(locationCircles);
