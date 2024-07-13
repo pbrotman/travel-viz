4ef6e24557ba7627f48130d6f1231c2b581ae762
pbrotman <parkerbrotman@gmail.com>
2024-07-13 18:02:50 -0400

    Generalize csv to json conv, process visit data.

--- data.py
+++ data.py
@@ -1,19 +1,18 @@
 import csv
 import json
 
-# Process locations data
-loc_csv_file = open("data/locations.csv", "r")
-loc_json_file = open("data/locations.json", "w")
+def csv_to_json(data_name, in_csv_path, out_json_path):
+    csv_file = open(in_csv_path, "r")
+    json_file = open(out_json_path, "w")
 
-locations_csv = csv.DictReader(loc_csv_file)
+    data_csv = csv.DictReader(csv_file)
 
-locations_list = []
-for row in locations_csv:
-    # print(row["Lat"], row["Lng"])
-    locations_list.append(row)
+    data_list = []
+    for row in data_csv:
+        data_list.append(row)
 
-print(locations_list)
+    data_json = json.dumps({data_name: data_list})
+    json_file.write(data_json)
 
-# locations_json = json.dumps({"locations": [row for row in locations_list]})
-locations_json = json.dumps({"locations": locations_list})
-loc_json_file.write(locations_json)
+csv_to_json("locations", "data/locations.csv", "data/locations.json")
+csv_to_json("visits", "data/visits.csv", "data/visits.json")
